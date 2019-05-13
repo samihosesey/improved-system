72f896d31a593308b178070a0c69858c7eb828d7
sami <hosesey.sami@gmail.com>
2019-05-14 00:44:12 +0300

    d
    q

--- footbalapi.py
+++ footbalapi.py
@@ -1,1 +1,18 @@
+import requests
+import json 
+from configparser import ConfigParser
 
+
+config = ConfigParser()
+
+config.read('config.ini')
+api = config.get('Api','api')
+uri = config.get('Api','uri')
+headers = config.get('Api','headers')
+plan = config.get('Api','plan')
+
+response = requests.get(api+"players/44")
+if response.status_code == 200:
+    print("200")
+else :
+    print("no")
